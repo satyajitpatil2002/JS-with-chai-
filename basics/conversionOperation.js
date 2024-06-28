215bfc85d059b7e10a9322895eeb3cb36ce7ee7a
satyajitpatil2002 <105158573+satyajitpatil2002@users.noreply.github.com>
2024-06-28 18:18:25 +0000

    some  confudsion operatins  handle successfully

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -23,3 +23,32 @@ let someNumber=22
 let stringNumber=String(someNumber) 
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+let value=3
+let negativeValue=-value
+console.log(negativeValue);
+
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(3/3);
+
+let str1="satyajit"
+let str2="patil"
+let str3=str1 + str2
+console.log(str3);
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+2+2);//>122
+console.log(1+2+"2")//output=33
+console.log(+true);//=>1
+console.log(+"")//=>0
+
+let num1,num2,num3
+num1=num2=num3=2+2;
+console.log(num3)
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
