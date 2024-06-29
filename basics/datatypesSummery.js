f13ff50bd94cd95357cc5012797e5f888d76f3f2
satyajitpatil2002 <105158573+satyajitpatil2002@users.noreply.github.com>
2024-06-29 04:59:07 +0000

    data types

---
+++ basics/datatypesSummery.js
@@ -0,0 +1,35 @@
+//primitive data types 
+
+//total 7 types: string,Number,Boolean,Null,Undefined,Symbol,BegInt
+
+const score=100
+const scoreValue=100.5
+const isLoggedOut=false;
+const outsideTemp=null;
+let userEmail;//undefined ;
+const id=Symbol('123')
+const anotherId= Symbol('123')
+console.log(id===anotherId)//false
+const bigNumber=123456678998481448n
+
+
+//js is an  daynamic languahe 
+
+//referance type (non primitive)
+//Array,Objects,Functions
+
+const heros=["shaktiman","nagaraj","doga"]
+let myObj={
+    name:"satya",
+    age:22,
+}
+
+const myFunstion=function(){
+    console.log("hello world");
+}
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures   dac8umentation
+
+
+
+
+
