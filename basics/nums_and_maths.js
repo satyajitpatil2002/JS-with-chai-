ccb68e9ef1e622445a5c81d0bf959f9eae931ab7
satyajitpatil2002 <105158573+satyajitpatil2002@users.noreply.github.com>
2024-06-29 11:47:26 +0000

    maths library commit

---
+++ basics/nums_and_maths.js
@@ -0,0 +1,41 @@
+// const score=400
+// console.log(score)
+
+// const balance=new Number(800)
+// console.log(balance)
+
+// // console.log (balance.toString().length)
+// console.log(typeof balance);
+// console.log(balance.toFixed(2));
+
+// const otherNumber=15.5481
+// console.log(otherNumber.toPrecision(3))//round off =>15.5
+
+// const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));//by default it gives amerian 1,000,000.....but for indian value we give 'en-IN' so it gives   10,00,000
+
+
+//++++++++++++++++++++++Maths+++++++++
+console.log(Math);
+
+// console.log(Math.abs(-4));//,ake positive or absulute value
+
+// console.log(Math.round(4.8));//roundup value
+// console.log(Math.ceil(4.8));//upper value 
+// console.log(Math.floor(4.8));//lower rounded valuen
+
+// console.log(Math.min(5,8,7,9,));
+// console.log(Math.max(7,8,6,4,9,7));
+
+console.log(Math.random());//always value in 0 to 1
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min=10;
+const max=20; 
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+//
+
+
+
+
