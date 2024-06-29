086fd5dd47b29b316fa98d6353d7bcc4d0fadbf4
satyajitpatil2002 <105158573+satyajitpatil2002@users.noreply.github.com>
2024-06-29 04:20:22 +0000

    some confusion camparions

---
+++ basics/camparison.js
@@ -0,0 +1,20 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2==1);
+console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);//false
+console.log(null== 0);//false
+console.log(null>=0);//true
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+//undefined gives all camparion false 
+
+//===
+console.log("2"==2);
+console.log("2"===2);//false because === check datatype equality also 
